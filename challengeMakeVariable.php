ae23dbf4423671154694a22b25c6cb1f40fe8a21
mandresyandri <manjess.andri@icloud.com>
2023-03-02 22:37:55 +0100

    update : stop works for 2-3

---
+++ challengeMakeVariable.php
@@ -0,0 +1,12 @@
+<?php
+
+/* declaring variables */ 
+
+$intNumber = 1000;
+$charName = 'N'; 
+$boolAccept = true; 
+$floatNum = 10.292;
+
+echo $intNumber.', '.  $charName.', '. $boolAccept.', '. $floatNum;
+
+?>
--- variableVariables.php
+++ variableVariables.php
@@ -6,4 +6,14 @@ $$cmw = 'm2cmw';
 
 echo ${$cmw};
 
+/* shows m2cmw */ 
+
+/* Set prefix variable */ 
+$mastercmw = 'm1, m2'; 
+$prefixvar = 'master'; 
+
+echo ${$prefixvar. 'cmw'};
+
+/* shows m1, m2 */ 
+
 ?>
